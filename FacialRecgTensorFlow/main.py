e34a882396537eea4e8d2584d69b23ae4a3b69a9
Kiarad <kiashadow2007@gmail.com>
2026-05-03 22:12:49 -0400

    make the model

--- FacialRecgTensorFlow/main.py
+++ FacialRecgTensorFlow/main.py
@@ -83,14 +83,37 @@ def make_embedding():
     c2 = Conv2D(128, (7,7), activation='relu')(m1)
     m2 = MaxPooling2D(64, (2,2), padding='same')(c2)
     
-    # Third block 
     c3 = Conv2D(128, (4,4), activation='relu')(m2)
     m3 = MaxPooling2D(64, (2,2), padding='same')(c3)
     
-    # Final embedding block
     c4 = Conv2D(256, (4,4), activation='relu')(m3)
     f1 = Flatten()(c4)
     d1 = Dense(4096, activation='sigmoid')(f1)
     
     
     return Model(inputs=[inp], outputs=[d1], name='embedding')
+
+class L1Dist(Layer):
+    
+    def __init__(self, **kwargs):
+        super().__init__()
+       
+    def call(self, input_embedding, validation_embedding):
+        return tf.math.abs(input_embedding - validation_embedding)
+
+def make_siamese_model(): 
+    
+    input_image = Input(name='input_img', shape=(100,100,3))
+    
+    validation_image = Input(name='validation_img', shape=(100,100,3))
+    
+    siamese_layer = L1Dist()
+    siamese_layer._name = 'distance'
+    distances = siamese_layer(embedding(input_image), embedding(validation_image))
+    
+    classifier = Dense(1, activation='sigmoid')(distances)
+    
+    return Model(inputs=[input_image, validation_image], outputs=classifier, name='SiameseNetwork')
+
+binary_cross_loss = tf.losses.BinaryCrossentropy()
+opt = tf.keras.optimizers.Adam(1e-4) 
