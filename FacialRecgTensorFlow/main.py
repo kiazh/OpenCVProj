aed76d8fa1601afdf19e4d5748c61da2e98353c1
Kiarad <kiashadow2007@gmail.com>
2026-05-03 20:28:43 -0400

    made preprocess function labeled datat etc

--- FacialRecgTensorFlow/main.py
+++ FacialRecgTensorFlow/main.py
@@ -44,3 +44,32 @@ anchor = tf.data.Dataset.list_files(ANC_PATH+'\*.jpg').take(3000)
 positive = tf.data.Dataset.list_files(POS_PATH+'\*.jpg').take(3000)
 negative = tf.data.Dataset.list_files(NEG_PATH+'\*.jpg').take(3000)
 dir_test = anchor.as_numpy_iterator()
+
+def preprocess(file_path):
+    byte_img = tf.io.read_file(file_path)
+    img = tf.io.decode_jpeg(byte_img)
+    
+    img = tf.image.resize(img, (100,100))
+    img = img / 255.0
+
+    return img
+
+positives = tf.data.Dataset.zip((anchor, positive, tf.data.Dataset.from_tensor_slices(tf.ones(len(anchor)))))
+negatives = tf.data.Dataset.zip((anchor, negative, tf.data.Dataset.from_tensor_slices(tf.zeros(len(anchor)))))
+data = positives.concatenate(negatives)
+
+def preprocess_twin(input_img, validation_img, label):
+    return(preprocess(input_img), preprocess(validation_img), label)
+
+data = data.map(preprocess_twin)
+data = data.cache()
+data = data.shuffle(buffer_size=10000)
+
+train_data = data.take(round(len(data)*.7))
+train_data = train_data.batch(16)
+train_data = train_data.prefetch(8)
+
+test_data = data.skip(round(len(data)*.7))
+test_data = test_data.take(round(len(data)*.3))
+test_data = test_data.batch(16)
+test_data = test_data.prefetch(8)
