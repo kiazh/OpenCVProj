f7c8a40c1a2ad5336879438118ede004a501a41a
Kiarad <kiashadow2007@gmail.com>
2026-05-03 21:59:39 -0400

    embedding

--- FacialRecgTensorFlow/main.py
+++ FacialRecgTensorFlow/main.py
@@ -73,3 +73,24 @@ test_data = data.skip(round(len(data)*.7))
 test_data = test_data.take(round(len(data)*.3))
 test_data = test_data.batch(16)
 test_data = test_data.prefetch(8)
+
+def make_embedding(): 
+    inp = Input(shape=(100,100,3), name='input_image')
+    
+    c1 = Conv2D(64, (10,10), activation='relu')(inp)
+    m1 = MaxPooling2D(64, (2,2), padding='same')(c1)
+    
+    c2 = Conv2D(128, (7,7), activation='relu')(m1)
+    m2 = MaxPooling2D(64, (2,2), padding='same')(c2)
+    
+    # Third block 
+    c3 = Conv2D(128, (4,4), activation='relu')(m2)
+    m3 = MaxPooling2D(64, (2,2), padding='same')(c3)
+    
+    # Final embedding block
+    c4 = Conv2D(256, (4,4), activation='relu')(m3)
+    f1 = Flatten()(c4)
+    d1 = Dense(4096, activation='sigmoid')(f1)
+    
+    
+    return Model(inputs=[inp], outputs=[d1], name='embedding')
